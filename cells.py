0cee66eae3a14a41b578aed4ca3c31065262cbd7
junzi <gigabayt@gmail.com>
2015-10-29 22:35:58 +0300

    temel olarak hucrelerin ozelliklerini tutan class eklendi

---
+++ cells.py
@@ -0,0 +1,34 @@
+from __future__ import division
+import math
+__author__ = 'junzi'
+
+#yazida once Y sonra X
+
+bitisx=474
+bitisy=360
+
+baslangicy=20
+baslangicx=30
+
+
+
+class cells:
+    def __init__(self):
+        self.x=0
+        self.y=0
+        self.dis_init=0
+        self.dis_end=0
+        self.find_dis_end()
+        self.find_dis_init()
+    def find_dis_end(self):
+        self.dis_end= math.sqrt(math.pow((self.x-bitisx),2)+math.pow((self.y-bitisy),2))
+        print "mesafe son ==>",self.dis_end
+
+    def find_dis_init(self):
+        self.dis_init= math.sqrt(math.pow((self.x-baslangicx),2)+math.pow((self.y-baslangicy),2))
+        print "mesafe baslangic==>",self.dis_init
+
+
+
+
+
