21c5aafe3fa321a4a084cc56ededef77be17c7f9
junzi <gigabayt@gmail.com>
2015-11-01 02:45:27 +0300

    komsular veren fonksiyon ve nokta_ekle nin farkli bir turu eklendi

--- numpy_dtype.py
+++ numpy_dtype.py
@@ -31,6 +31,44 @@ def nokta_ekle(kordinatlar,x,y):
     kordinatlar[kordinatlar.shape[0]-1]['y']=y
     return kordinatlar
 
+def nokta_ekle2(kordinatlar,sayilar):
+    x=sayilar[0]
+    y=sayilar[1]
+    array=kordinatlar.copy()
+    kordinatlar = np.empty( kordinatlar.shape[0]+1, dtype=array.dtype)
+    kordinatlar[0:kordinatlar.shape[0]-1]=array.copy()
+    kordinatlar[kordinatlar.shape[0]-1]['x']=x
+    kordinatlar[kordinatlar.shape[0]-1]['y']=y
+    return kordinatlar
+
+def komsular(a):
+    x=a[0]
+    y=a[1]
+    kordinatlar = np.empty( 8, dtype=data_type_kordinatlar)
+    kordinatlar[0]['x']=x-1
+    kordinatlar[0]['y']=y
+
+    kordinatlar[1]['x']=x-1
+    kordinatlar[1]['y']=y-1
+
+    kordinatlar[2]['x']=x
+    kordinatlar[2]['y']=y-1
+
+    kordinatlar[3]['x']=x+1
+    kordinatlar[3]['y']=y-1
+
+    kordinatlar[4]['x']=x+1
+    kordinatlar[4]['y']=y
+
+    kordinatlar[5]['x']=x+1
+    kordinatlar[5]['y']=y+1
+
+    kordinatlar[6]['x']=x
+    kordinatlar[6]['y']=y+1
+
+    kordinatlar[7]['x']=x-1
+    kordinatlar[7]['y']=y+1
+    return kordinatlar
 
 def mesafe_yaz(mesafe,kordinat):
     if mesafe.shape[0]!=kordinat.shape[0]:
@@ -41,3 +79,5 @@ def mesafe_yaz(mesafe,kordinat):
         mesafe[a]['mes_baslangic']= math.sqrt(math.pow((kordinat[a]['x']-baslangicx), 2)+math.pow((kordinat[a]['y']-baslangicy), 2))
         mesafe[a]['toplam']=mesafe[a]['mes_son']+mesafe[a]['mes_baslangic']
     return mesafe
+
+
