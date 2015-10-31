d1f10539592d54fd3a5d0071dc6cf69bc9ef36c3
junzi <gigabayt@gmail.com>
2015-10-31 22:43:47 +0300

    mesafeleri alan fonksiyon eklendi
    array mevcut noktalarin hem baslangic mesafesini hemde bitis mesafesini kaydediyor

--- numpy_dtype.py
+++ numpy_dtype.py
@@ -19,12 +19,8 @@ kordinatlar = np.empty( 5, dtype=data_type_kordinatlar)
 
 mesafeler=np.empty( 5, dtype=data_type_mesafeler)
 
+kordinatlar[:]=474,360
 
-#mesafeler['mes_son']= math.sqrt(math.pow((kordinatlar['x']-bitisx),2)+math.pow((kordinatlar['x']-bitisy),2))
-
-
-kordinatlar[:]=12,13
-kordinatlar[0]=1,2
 x=3
 y=5
 
@@ -32,11 +28,20 @@ y=5
 def nokta_ekle(kordinatlar,x,y):
     array=kordinatlar.copy()
     kordinatlar = np.empty( kordinatlar.shape[0]+1, dtype=array.dtype)
-    print kordinatlar.shape[0]
-
     kordinatlar[0:kordinatlar.shape[0]-1]=array.copy()
     kordinatlar[kordinatlar.shape[0]-1]['x']=x
     kordinatlar[kordinatlar.shape[0]-1]['y']=y
     return kordinatlar
 
-mesafe_yaz(mesafeler,kordinatlar)
+
+def mesafe_yaz(mesafe,kordinat):
+    if mesafe.shape[0]!=kordinat.shape[0]:
+        mesafe=np.empty( kordinat.shape[0], dtype=mesafe.dtype)
+
+    for a in range(0,mesafe.shape[0]):
+        mesafe[a]['mes_son']= math.sqrt(math.pow((kordinat[a]['x']-bitisx), 2)+math.pow((kordinat[a]['y']-bitisy), 2))
+        mesafe[a]['mes_baslangic']= math.sqrt(math.pow((kordinat[a]['x']-baslangicx), 2)+math.pow((kordinat[a]['y']-baslangicy), 2))
+    return mesafe
+
+
+
